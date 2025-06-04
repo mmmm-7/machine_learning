8fa1ae279eaaa599734d7773418cdfb92d1d6eb8
mmmm-7 <sflsmj@126.com>
2025-06-04 20:36:30 +0800

    home.py

--- page_content/home.py
+++ page_content/home.py
@@ -16,7 +16,7 @@ def home_page(): # add a function to display the home page in streamlit app
     )
 
     # add a photo to the right column
-    image_path = os.path.join("static", "images", "image.png")
+    image_path = os.path.join("static", "images", "image.jpg")
     if os.path.exists(image_path):
         image = Image.open(image_path)
         right_col.image(image, width=200)
